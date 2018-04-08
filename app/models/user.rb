81013a0993b33e600a4c2c9dcc519835da773c1d
Thiago-Cardoso <trovador182@gmail.com>
2018-04-08 20:31:49 -0300

    adjustment for send email in api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Include default devise modules.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-  #link user with form, when delete a user, destroy all forms
+
+  
   has_many :forms, dependent: :destroy
 end
